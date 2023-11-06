8b9aa32319cdb38f687bf54d8193674d1b1cd26e
Eugene Damiba <eugene.t.damiba@intel.com>
2023-11-06 08:28:55 -0800

    Adds feature request to expose IXL device information
    Signed-off-by: Eugene Damiba <eugene.t.damiba@intel.com>

--- compress/ctx.go
+++ compress/ctx.go
@@ -4,10 +4,34 @@
 package compress
 
 import (
+	"github.com/intel/ixl-go/internal/config"
 	"github.com/intel/ixl-go/internal/iaa"
 )
+
+type iaadevice struct {
+	ID            uint64 // Device ID.
+	NumaNode      int    // NUMA node.
+	NumWorkQueues int    // Number of work queues.
+}
 
 // Ready checks if the hardware is usable.
 func Ready() bool {
 	return iaa.LoadContext().Ready()
+}
+
+func GetDevices() []iaadevice {
+	ctx := config.NewContext()
+	ctx.Engines(config.IAA)
+	var iaadevices []iaadevice
+
+	for _, d := range ctx.Devices {
+		if d.Type != config.IAA {
+			continue
+		}
+		if d.State == config.DeviceStateEnabled {
+			newd := iaadevice{ID: d.ID, NumaNode: d.NumaNode, NumWorkQueues: len(d.WorkQueues)}
+			iaadevices = append(iaadevices, newd)
+		}
+	}
+	return iaadevices
 }
